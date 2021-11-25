979c4bc643164265dd866df2ddf7b353aa537888
Ricky <55218898+Dosrui78@users.noreply.github.com>
2021-11-25 15:52:52 +0800

    Update Spider.js

--- Spider.js
+++ Spider.js
@@ -1,11 +1,7 @@
 const XLSX = require('node-xlsx')
 const puppeteer = require('puppeteer')
 const mysql = require('mysql')
-<<<<<<< HEAD
-var schoolName = '重庆文理学院'
-=======
-var schoolName = '重庆第二师范学院'
->>>>>>> 514ff32 (更换成xpath解析)
+var schoolName = '南京邮电大学'
 var excelFilePath = `./${schoolName}.xlsx`
 
 
@@ -27,19 +23,11 @@ const browser = puppeteer.launch({
 
 
 var conn = mysql.createConnection({
-<<<<<<< HEAD
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'teacher'
-    })
-=======
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'teacher'
 })
->>>>>>> 514ff32 (更换成xpath解析)
 conn.connect()
 
 function sleep(ms) {
@@ -47,40 +35,26 @@ function sleep(ms) {
 }
 
 function saveData(schoolName, academy, name, title, faculty, direction, curl) {
-<<<<<<< HEAD
-
-=======
->>>>>>> 514ff32 (更换成xpath解析)
     // conn.connect(function (err){if(err){
     //     console.error('error connecting: ' + err.stack);
     //     return;
     // }})
 
     // console.log('connected as id ' + conn.threadID)
+    try {
+        let sql = `INSERT INTO teacher(school, faculty, name, title, subject, curriculum, introduction)
+                   values ('${schoolName}', '${academy}', '${name}', '${title}', '${faculty}', '${direction}', '${curl}
+                           ')`
+        conn.query(sql)
+    }
+    catch{
+        conn.rollback()
+        console.log("wirte error")
+    }
 
-    let sql = `INSERT INTO teacher(school, faculty, name, title, subject, curriculum, introduction)
-               values ('${schoolName}', '${academy}', '${name}', '${title}', '${faculty}', '${direction}', '${curl}')`
-    conn.query(sql)
 
 }
 
-<<<<<<< HEAD
-
-
-async function getTexts(sel, page) {
-    zz = await page.$$eval(sel, el => el.map(el => el.innerText))
-    return zz
-}
-
-async function getText(sel, page) {
-    zz = await page.$eval(sel, el => el.textContent)
-    return zz
-}
-
-async function href(sel, page){
-    zz = await page.$$eval(sel, el => el.map(el => el.href))
-    return zz
-=======
 async function getText(zz) {
     /* Xpath解析 */
 
@@ -95,7 +69,6 @@ async function getHref(sel) {
     const zz = await Promise.all(sel.map(handle => handle.getProperty('href')));
     zzz = await Promise.all(zz.map(handle => handle.jsonValue()));
     return zzz
->>>>>>> 514ff32 (更换成xpath解析)
 }
 
 function unique(arr) {
@@ -104,15 +77,7 @@ function unique(arr) {
 
 async function judgeUrl(s) {
     /* url去重以及判断抓取字段是否有遍历 */
-<<<<<<< HEAD
-    if (s[2] !== 1) {
-        s[4] = s[2] + s[4];
-        s[5] = s[2] + s[5];
-        s[6] = s[2] + s[6];
-    }
-=======
 
->>>>>>> 514ff32 (更换成xpath解析)
     let arr = []
     for (const b of s[1].split('\n')) {
         arr.push(b.replace('\r', ''));
@@ -123,55 +88,6 @@ async function judgeUrl(s) {
 
 async function judgeUrl1(s, page, url) {
     /*判断url是否能正常访问*/
-<<<<<<< HEAD
-    let x = await getTexts(s, page)
-    if (x.length === 0 || x === '') {
-        console.error(`Error: url is not right! ${url}`)
-    }
-}
-
-async function one(content, i) {
-    if (content.length === 1) {
-        content = content[0]
-        if(content === undefined || content === null){content = "";} //避免储存的值为null
-    }else {content = content[i];
-    if(content === undefined || content === null){content = "";}}
-    return content
-}
-
-
-async function judgeSel(content, page){
-    // console.log((await getTexts(content, page)))
-    if(content && (await getTexts(content, page)).length === 0)
-{
-        console.log(`Error: Selector is wrong!! ${content}`)
-    }
-}
-
-async function getData(s, page) {
-    /* 解析，获取数据 */
-    let temp = []
-    var academy = s[0];
-    var name, title, subject, direction, curl = [];
-    names = await getTexts(s[3], page);
-    titles = await getTexts(s[4], page);
-    subjects = await getTexts(s[5], page);
-    directions = await getTexts(s[6], page);
-    curls = await href(s[3], page);
-    temp.push(s[3], s[4], s[5], s[6])
-    for(const t of temp){
-        await judgeSel(t, page);
-    }
-    for (let i = 0; i < names.length; i++) {
-        name = names[i];
-        title = await one(titles, i);
-        subject = await one(subjects, i);
-        direction = await one(directions, i);
-        curl = await one(curls, i);
-        console.log(name, title, subject, direction, curl);
-        await saveData(schoolName, academy, name, title, subject, direction, curl)
-    }
-=======
     let tm = await page.$eval('body', el => el.innerText)
     if (tm.indexOf('Not Found') !== -1 || tm.indexOf('您访问的页面未找到， 5秒后自动跳转到首页') !== -1) {
         console.log(`Error: Url is wrong! ${url}`);
@@ -235,10 +151,9 @@ async function getData(s, page, count) {
         subject = await zero(subjects[i][0]);
         direction = await zero(directions[i][0]);
         console.log(schoolName, academy, name, title, subject, direction, curl)
-        // await saveData(schoolName, academy, name, title, subject, direction, curl)
+        await saveData(schoolName, academy, name, title, subject, direction, curl)
     }
     return count
->>>>>>> 514ff32 (更换成xpath解析)
 }
 
 /* s[0]是学院名称；s[1]是学院链接；s[2]作为判断是否每个人的其他信息都是单独列出来的依据；
@@ -246,10 +161,7 @@ async function getData(s, page, count) {
  * 师详情页。
  *  */
 (async () => {
-<<<<<<< HEAD
-=======
     let count = 0;
->>>>>>> 514ff32 (更换成xpath解析)
     const page = await (await browser).newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
     const sheets = XLSX.parse(excelFilePath) //解析excel，获取到所有sheets
@@ -257,32 +169,17 @@ async function getData(s, page, count) {
     for (const s of sheet.data) {
         const arr = await judgeUrl(s)
         for (const a of arr) {
-<<<<<<< HEAD
-            // console.log(a)
-            await page.goto(a, {waitUntil: 'networkidle2'}).catch(async e => console.log(e));
-            await judgeUrl1(s[3], page, a);
-            await getData(s, page);
-            await sleep(600);
-        }
-    }
-=======
             await page.goto(a, {waitUntil: 'networkidle2'}).catch(async e => console.log(e));
             const bool = await judgeUrl1(s[3], page, a);  //检测url
             if (bool !== -1) {
                 count = await getData(s, page, count);
-                await sleep(600);
+                await sleep(200);
             }
         }
     }
     console.log(`共抓到${count}条数据`)
->>>>>>> 514ff32 (更换成xpath解析)
     // await (await browser).close();
 })()
 
 
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 514ff32 (更换成xpath解析)
